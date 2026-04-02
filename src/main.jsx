0011628a2a76f2ae9e375b6d07499a7557d648e4
ashirbadsamal78 <ashirbad.samal@outlook.com>
2026-04-03 02:34:33 +0530

    Delete src directory

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import VoltasMaintenancePortal from './VoltasMaintenancePortal'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <VoltasMaintenancePortal />
-  </React.StrictMode>
-)
