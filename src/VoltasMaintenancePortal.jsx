8ed164244e6b64a1b33a3c9ec7121a740e908282
ashirbadsamal78 <ashirbad.samal@outlook.com>
2026-04-03 02:01:48 +0530

    Create VoltasMaintenancePortal.jsx

---
+++ src/VoltasMaintenancePortal.jsx
@@ -0,0 +1,8 @@
+export default function VoltasMaintenancePortal() {
+  return (
+    <div style={{padding:20,fontFamily:'Arial'}}>
+      <h1>VOLTAS Maintenance System</h1>
+      <p>System is running successfully.</p>
+    </div>
+  );
+}
