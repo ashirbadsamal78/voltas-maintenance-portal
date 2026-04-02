66be1c61cd8ea7f8539edceba3fd52ce53316df0
ashirbadsamal78 <ashirbad.samal@outlook.com>
2026-04-03 02:00:52 +0530

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import VoltasMaintenancePortal from './VoltasMaintenancePortal'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <VoltasMaintenancePortal />
+  </React.StrictMode>
+)
